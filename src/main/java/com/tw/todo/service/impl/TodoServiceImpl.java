cb6cdc131aaaad8bbb1fd09b41ecb1e22a597fb5
wyuefzhuol <1527169088@qq.com>
2020-08-06 20:17:00 +0800

    [todo][Oliver&Eric] feature: pass should_return_1_todo_item_response_when_add_todo_item_given_1_todo_item_request

--- src/main/java/com/tw/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/tw/todo/service/impl/TodoServiceImpl.java
@@ -2,8 +2,10 @@ package com.tw.todo.service.impl;
 
 import com.tw.todo.dto.TodoItemRequest;
 import com.tw.todo.dto.TodoItemResponse;
+import com.tw.todo.entity.TodoItem;
 import com.tw.todo.repository.TodoRepository;
 import com.tw.todo.service.TodoService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,10 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public TodoItemResponse addTodoItem(TodoItemRequest todoItemRequest) {
-        return null;
+        TodoItem todoItem = new TodoItem();
+        TodoItemResponse todoItemResponse = new TodoItemResponse();
+        BeanUtils.copyProperties(todoItemRequest, todoItem);
+        BeanUtils.copyProperties(todoRepository.save(todoItem), todoItemResponse);
+        return todoItemResponse;
     }
 }
