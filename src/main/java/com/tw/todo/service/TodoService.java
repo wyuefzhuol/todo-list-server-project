cf202b43796b476772689af4a21a2b60d4832c5d
wyuefzhuol <1527169088@qq.com>
2020-08-06 20:25:11 +0800

    [todo][Oliver&Eric] feature: pass should_return_1_todo_item_response_when_get_todo_items_given_1_todo_item

--- src/main/java/com/tw/todo/service/TodoService.java
+++ src/main/java/com/tw/todo/service/TodoService.java
@@ -3,6 +3,10 @@ package com.tw.todo.service;
 import com.tw.todo.dto.TodoItemRequest;
 import com.tw.todo.dto.TodoItemResponse;
 
+import java.util.List;
+
 public interface TodoService {
     TodoItemResponse addTodoItem(TodoItemRequest todoItemRequest);
+
+    List<TodoItemResponse> getTodoItems();
 }
